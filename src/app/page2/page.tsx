0916061ec45301ab4ea2afee1588cf225baeb152
Maximo Ramirez <a00839731@tec.mx>
2025-03-10 07:39:30 -0600

    Update page.tsx

--- src/app/page2/page.tsx
+++ src/app/page2/page.tsx
@@ -17,7 +17,7 @@ export default function Hook() {
       <header>
         <nav>
           <ul>
-            <li><Link href="/">Home</Link>
+            <li><Link href="/">Home</Link></li>
             <li><a href="/page2">Hook</a></li>
           </ul>
         </nav>
